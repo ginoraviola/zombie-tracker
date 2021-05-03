82d441f0a389dd764a6bf345e8040c8ede812dce
Gino Raviola <ginoraviola@gmail.com>
2021-05-02 23:04:34 -0300

    prettier suggestions

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,9 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://192.168.1.22:5000' });
+const API = axios.create({baseURL: 'http://192.168.1.119:5000'});
 
 export const getLocations = () => API.get('/locations');
-export const getLocationInfo = (id) => API.get(`/location/${id}`);
-export const removeZombie = (data) => API.post('/remove', data);
-export const relocateZombie = (data) => API.post('/relocate', data);
-
+export const getLocationInfo = id => API.get(`/location/${id}`);
+export const removeZombie = data => API.post('/remove', data);
+export const relocateZombie = data => API.post('/relocate', data);
